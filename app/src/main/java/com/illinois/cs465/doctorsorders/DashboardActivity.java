b9924258d397c5ed08c77cda59f235437ee8a138
czhou578 <colizu2020@gmail.com>
2022-11-03 12:12:58 -0500

    Button that leads to caretaker dashboard

---
+++ app/src/main/java/com/illinois/cs465/doctorsorders/DashboardActivity.java
@@ -0,0 +1,11 @@
+package com.illinois.cs465.doctorsorders;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.caretaker_dashboard_layout);
+    }
+}
--- app/src/main/java/com/illinois/cs465/doctorsorders/MainActivity.java
+++ app/src/main/java/com/illinois/cs465/doctorsorders/MainActivity.java
@@ -2,13 +2,38 @@ package com.illinois.cs465.doctorsorders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button dashboardBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dashboardBtn = (Button) findViewById(R.id.dashboardbtn);
+        dashboardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+            }
+        });
     }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.dashboardbtn) {
+            Toast.makeText(this, "Right", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Wrong", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+//    public void openDashboard() {
+//        Intent intent = new Intent(this, New)
+//    }
 }
