55a461c162e63c10c74b3b79dc8275831d44f5bb
Cheruku Deeksha <deeksha.cheruku@gmail.com>
2022-11-07 16:36:13 -0600

    Update MainActivity.java
    Added functionality to patient Button

--- app/src/main/java/com/illinois/cs465/doctorsorders/MainActivity.java
+++ app/src/main/java/com/illinois/cs465/doctorsorders/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button dashboardBtn = findViewById(R.id.dashboardbtn);
         dashboardBtn.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, DashboardActivity.class)));
+
+        Button patientBtn = findViewById(R.id.patient_btn);
+        patientBtn.setOnClickListener(view -> Toast.makeText(getApplicationContext(), "Patient", Toast.LENGTH_LONG).show());
     }
 }
