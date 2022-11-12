4aca3d67b7a2466094d28d0ad665569ebf7f6856
czhou578 <colizu2020@gmail.com>
2022-11-12 13:23:33 -0600

    finished layout for set schedule screen.

--- app/src/main/java/com/illinois/cs465/doctorsorders/set_medication_1.java
+++ app/src/main/java/com/illinois/cs465/doctorsorders/set_medication_1.java
@@ -2,7 +2,9 @@ package com.illinois.cs465.doctorsorders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class set_medication_1 extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class set_medication_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_medication1);
+
+        Button button = (Button) findViewById(R.id.next_to_schedule);
+        button.setOnClickListener(view -> startActivity(new Intent(set_medication_1.this, set_schedule2.class)));
     }
 }
