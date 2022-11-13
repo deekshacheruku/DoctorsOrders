e3fed854f9fc03caab84115edb6991d475354068
czhou578 <colizu2020@gmail.com>
2022-11-13 08:54:41 -0600

    Finished layout for confirm schedule

---
+++ app/src/main/java/com/illinois/cs465/doctorsorders/confirm_medication_3.java
@@ -0,0 +1,14 @@
+package com.illinois.cs465.doctorsorders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class confirm_medication_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_medication3);
+    }
+}
--- app/src/main/java/com/illinois/cs465/doctorsorders/set_schedule2.java
+++ app/src/main/java/com/illinois/cs465/doctorsorders/set_schedule2.java
@@ -2,7 +2,9 @@ package com.illinois.cs465.doctorsorders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class set_schedule2 extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class set_schedule2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_schedule2);
+        Button button = (Button) findViewById(R.id.next_to_confirm);
+        button.setOnClickListener(view -> startActivity(new Intent(set_schedule2.this, confirm_medication_3.class)));
+
     }
 }
