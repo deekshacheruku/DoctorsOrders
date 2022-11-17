1a17f072688785bb472fd48eaf160a27af35e490
Cheruku Deeksha <deeksha.cheruku@gmail.com>
2022-11-17 02:34:01 -0600

    Fixed merge conflicts

--- app/src/main/java/com/illinois/cs465/doctorsorders/PatientDashboardActivity.java
+++ app/src/main/java/com/illinois/cs465/doctorsorders/PatientDashboardActivity.java
@@ -2,6 +2,7 @@ package com.illinois.cs465.doctorsorders;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +11,9 @@ import java.time.LocalTime;
 import java.util.Random;
 
 public class PatientDashboardActivity extends AppCompatActivity {
-    String[] medicines = { "Atorvastatin", "Metformin", "Simvastatin", "Omeprazole", "Amlodipine", "Metoprolol" };
+    String[] medicines = {"Atorvastatin", "Metformin", "Simvastatin", "Omeprazole", "Amlodipine"};
+    int[] images = {R.drawable.atorvastatin, R.drawable.metformin, R.drawable.simvastatin,
+            R.drawable.omeprazole, R.drawable.amlodipine};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +21,17 @@ public class PatientDashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_patient_dashboard_layout);
         int randomMedicine = new Random().nextInt(medicines.length);
 
-        TextView tv = (TextView)findViewById(R.id.patient_medicine);
-        tv.setText(getString(R.string.patient_medicine,medicines[randomMedicine]));
+        TextView tv = (TextView) findViewById(R.id.patient_medicine);
+        tv.setText(getString(R.string.patient_medicine, medicines[randomMedicine]));
 
         TextView time = findViewById(R.id.patient_medicine_time);
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             String t = String.valueOf(LocalTime.now().getHour());
             t = t + ":" + LocalTime.now().plusMinutes(1).getMinute();
             time.setText(getString(R.string.patient_medicine_time, t));
         }
+
+        ImageView image = findViewById(R.id.patient_medicine_image);
+        image.setImageResource(images[randomMedicine]);
     }
 }
