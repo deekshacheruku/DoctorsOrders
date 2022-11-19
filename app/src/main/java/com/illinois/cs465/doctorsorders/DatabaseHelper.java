a9937b414f0991bf6c031f59596c5b34f507f8fe
czhou578 <colizu2020@gmail.com>
2022-11-19 14:03:40 -0600

    database insert working!

---
+++ app/src/main/java/com/illinois/cs465/doctorsorders/DatabaseHelper.java
@@ -0,0 +1,52 @@
+package com.illinois.cs465.doctorsorders;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "DoctorsOrders.db";
+
+    private static final String TABLE_NAME = "Scheduler_Patients_List";
+    private static final String COL1 = "Scheduler_Name";
+    private static final String COL2 = "Patient_First_Name";
+    private static final String COL3 = "Patient_Last_Name";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL1 + " TEXT, " + COL2 + " TEXT, " + COL3 + " TEXT)";
+        db.execSQL(createTable);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addNearbyPatient(Bundle bundle) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL1, bundle.getString("scheduler_name"));
+        contentValues.put(COL2, bundle.getString("patient_first_name"));
+        contentValues.put(COL3, bundle.getString("patient_last_name"));
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) {
+            Log.d("-1", "this is false");
+            return false;
+        } else {
+            Log.d("1", "this is true");
+            return true;
+        }
+    }
+}
--- app/src/main/java/com/illinois/cs465/doctorsorders/NearbyPatients.java
+++ app/src/main/java/com/illinois/cs465/doctorsorders/NearbyPatients.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -11,11 +12,14 @@ import android.widget.ListView;
 public class NearbyPatients extends AppCompatActivity {
     String patientName[] = {"Dave Woods", "Cathie Hodges", "Jim Frost"};
     int profilePic[] = {R.drawable.profile_pic};
+    DatabaseHelper databaseHelper;
 
     ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        databaseHelper = new DatabaseHelper(this);
+
         setContentView(R.layout.activity_nearby_patients);
         listView = (ListView) findViewById(R.id.patientList);
         ProfileAdapter adapter = new ProfileAdapter(getApplicationContext(), patientName);
@@ -24,7 +28,16 @@ public class NearbyPatients extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Object name = adapterView.getItemAtPosition(i);
+                Object clickedName = adapterView.getItemAtPosition(i);
+                String[] name = clickedName.toString().split("\\s+");
+
+                Bundle dbInsertBundle = new Bundle();
+                dbInsertBundle.putString("patient_first_name", name[0]);
+                dbInsertBundle.putString("patient_last_name", name[1]);
+                dbInsertBundle.putString("scheduler_name", "David Smith");
+
+                databaseHelper.addNearbyPatient(dbInsertBundle);
+
                 Intent intent = new Intent(NearbyPatients.this, DashboardActivity.class);
                 startActivity(intent);
             }
